53749898f4dff14de00254e47031d7c209a77241
Thiago Silva Lopes <thiagodrive08@hotmail.com>
2021-09-20 18:38:15 -0300

    Indo pra testando botão :)

--- components/atoms/Button/Button.spec.ts
+++ components/atoms/Button/Button.spec.ts
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import Button from '@/components/atoms/Button/Button.vue'
 
-describe('NuxtLogo', () => {
+describe('Button', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(Button, {
       propsData: {
